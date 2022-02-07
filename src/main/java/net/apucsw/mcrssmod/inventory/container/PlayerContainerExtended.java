45ba3bd7d2f3e08552f18d9913401d71c5df4182
korAPUcard <tmdrnr1157@gmail.com>
2022-02-08 07:12:49 +0900

    Git update

---
+++ src/main/java/net/apucsw/mcrssmod/inventory/container/PlayerContainerExtended.java
@@ -0,0 +1,8 @@
+package net.apucsw.mcrssmod.inventory.container;
+
+import net.minecraft.inventory.container.PlayerContainer;
+import net.minecraft.util.ResourceLocation;
+
+public class PlayerContainerExtended {
+    public static final ResourceLocation EMPTY_WEARABLE_DEVICE_SLOT = new ResourceLocation("items/empty_wearable_device_slot");
+}
---
+++ src/main/java/net/apucsw/mcrssmod/inventory/container/slot/InventoryWearableDeviceSlot.java
@@ -0,0 +1,41 @@
+/**
+ * The code of this mod element is always locked.
+ *
+ * You can register new events in this class too.
+ *
+ * If you want to make a plain independent class, create it using
+ * Project Browser -> New... and make sure to make the class
+ * outside net.apucsw.mcrssmod as this package is managed by MCreator.
+ *
+ * If you change workspace package, modid or prefix, you will need
+ * to manually adapt this file to these changes or remake it.
+ *
+ * This class will be added in the mod root package.
+*/
+package net.apucsw.mcrssmod.inventory.container.slot;
+
+import com.mojang.datafixers.util.Pair;
+import net.apucsw.mcrssmod.McrssmodMod;
+import net.apucsw.mcrssmod.inventory.container.PlayerContainerExtended;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+
+
+public class InventoryWearableDeviceSlot extends Slot {
+	public InventoryWearableDeviceSlot(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+		super(inventoryIn, index, xPosition, yPosition);
+	}
+
+	@Nullable
+	//@Override
+	@OnlyIn(Dist.CLIENT)
+	public Pair<ResourceLocation, ResourceLocation> getNoItemIcon() {
+		return Pair.of(AtlasTexture.LOCATION_BLOCKS_TEXTURE, PlayerContainerExtended.EMPTY_WEARABLE_DEVICE_SLOT);
+	}
+}
