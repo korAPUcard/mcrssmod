a3e669677a6dc96cadde30d8605f74ad5a07fa09
korAPUcard <tmdrnr1157@gmail.com>
2022-02-14 20:26:16 +0900

    Git update

---
+++ src/main/java/net/apucsw/mcrssmod/procedures/ModDebuggerGUIProcedure.java
@@ -0,0 +1,10 @@
+package net.apucsw.mcrssmod.procedures;
+
+import java.util.Map;
+
+public class ModDebuggerGUIProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
---
+++ src/main/java/net/apucsw/mcrssmod/procedures/ModDebuggerTargetBlockProcedure.java
@@ -0,0 +1,10 @@
+package net.apucsw.mcrssmod.procedures;
+
+import java.util.Map;
+
+public class ModDebuggerTargetBlockProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
