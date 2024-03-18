738a77d02cadeafa40a7289b32f279bc25e24782
korAPUcard <tmdrnr1157@gmail.com>
2024-03-18 11:01:52 +0900

    1.20.4 Initial Commit

--- src/main/java/net/apucsw/mcrssmod/RSSMod.java
+++ src/main/java/net/apucsw/mcrssmod/RSSMod.java
@@ -1,5 +1,4 @@
-package com.example.examplemod;
-
+package net.apucsw.mcrssmod;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
@@ -30,11 +29,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(RSSMod.MODID)
+public class RSSMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mcrssmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -61,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public RSSMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
